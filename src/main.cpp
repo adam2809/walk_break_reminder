c1b6f811cce71b77733042b9a003447bd460d947
adam2809 <adam.kule@gmail.com>
2020-12-31 16:14:49 +0100

    Tried to do multipart request as payload of normal request

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,32 @@
 
 #define STRAVA_API_URL "https://www.strava.com/api/v3"
 #define AUTH_HEADER "Authorization", "Bearer " + String(authToken)
+#define ACCEPT_HEADER "Accept", "*/*"
+#define ACCEPT_ENCODING_HEADER "Accept-Encoding", "gzip, deflate"
+#define CONNECTION_HEADER "Connection", "keep-alive"
+#define HOST_HEADER "Host", "www.strava.com"
+#define USER_AGENT_HEADER "User-Agent", "ESP32StravaTracker/0.0.1"
+#define MULTIPART_BOUNDARY "ESP32StravaTrackeraf39cf00aaaa"
+#define MULTIPART_CONTENT_TYPE_HEADER "Content-Type", "multipart/form-data; boundary=" + String(MULTIPART_BOUNDARY)
+#define MULTIPART_FORM_KEY_PREFIX "Content-Disposition: form-data; name="
+
+String uploadActivityFormKeys[] = {
+    "name",
+    "description",
+    "trainer",
+    "commute",
+    "data_type",
+    "external_id",
+    "file",
+};
 
 const char* authToken;
 char stravaCa[2048];
 HTTPClient http;
 StaticJsonBuffer<1024> jsonBuffer;
 String readBuffer;
+String uploadPayload;
+char testGpx[4096];
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     Serial.printf("Listing directory: %s\r\n", dirname);
@@ -90,7 +110,7 @@ void performStravaApiRequest(const char* method,String endpoint,String payload,S
         http.addHeader(headers[i], headers[i+1]);
     }
 
-	int httpCode = http.sendRequest(method,payload);
+	int httpCode = http.sendRequest(method,uploadPayload);
 
 	if (httpCode > 0) {
 		String payload = http.getString();
@@ -103,6 +123,34 @@ void performStravaApiRequest(const char* method,String endpoint,String payload,S
 
 	http.end();
 }
+
+void makeUploadPayload(){
+    String uploadActivityFormVals[] = {
+        "tescisko",
+        "tesciskownie aktywne",
+        "true",
+        "false",
+        "gpx",
+        "ext_id",
+        testGpx
+    };
+
+    for(int i=0;i<7;i++){
+        String key = uploadActivityFormKeys[i];
+        String val = uploadActivityFormVals[i];
+
+        
+        uploadPayload.concat("--"+String(MULTIPART_BOUNDARY));uploadPayload.concat("\r\n");
+        uploadPayload.concat(String(MULTIPART_FORM_KEY_PREFIX) + "\""+ key+"\"");if(key="file") uploadPayload.concat("; filename=\"test.gpx\"");uploadPayload.concat("\r\n");
+        uploadPayload.concat("\r\n");
+        uploadPayload.concat(val);uploadPayload.concat("\r\n");
+    }
+    uploadPayload.concat("\r\n");
+    uploadPayload.concat("--"+String(MULTIPART_BOUNDARY)+"--");uploadPayload.concat("\r\n");
+    uploadPayload.concat("\r\n");
+}
+
+
 
 void setup() {
 	Serial.begin(115200);
@@ -124,20 +172,38 @@ void setup() {
     authToken = config["auth_token"];
     Serial.print("Auth token is: ");Serial.println(authToken);
 
+
+    if(!readFile(SPIFFS, "/test.gpx")){
+        Serial.println("Could not test gpx file");
+    }
+    readBuffer.toCharArray(testGpx,readBuffer.length());
+
 	connectToWiFi(config["ssid"],config["password"]);
 
-	Serial.println("Performing strava request");
+    makeUploadPayload();
+    Serial.println(uploadPayload);
 
     String getAthleteInfoHeaders[2] = {
         AUTH_HEADER
     };
+    String uploadActivityHeaders[16] = {
+        ACCEPT_HEADER,
+        ACCEPT_ENCODING_HEADER,
+        AUTH_HEADER,
+        CONNECTION_HEADER,
+        MULTIPART_CONTENT_TYPE_HEADER,
+        HOST_HEADER,
+        USER_AGENT_HEADER
+    };
 
+
+	Serial.println("Performing strava request");
 	performStravaApiRequest(
-        "GET",
-        "/athlete",
-        "",
-        getAthleteInfoHeaders,
-        1
+        "POST",
+        "/uploads",
+        uploadPayload,
+        uploadActivityHeaders,
+        7
     );
 }
 
