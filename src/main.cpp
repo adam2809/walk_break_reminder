af771c589642240ac67651400d7184dec245e38b
adam2809 <adam.kule@gmail.com>
2021-01-10 14:58:49 +0100

    Version from which strava question was asked

--- src/main.cpp
+++ src/main.cpp
@@ -1,169 +1,92 @@
-#include <Arduino.h>
-#include <HTTPClient.h>
-#include <WiFi.h>
-#include <SPIFFS.h>
-#include <ArduinoJson.h>
+/*
+  Wifi secure connection example for ESP32
+  Running on TLS 1.2 using mbedTLS
+  Suporting the following chipersuites:
+  "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384","TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384","TLS_DHE_RSA_WITH_AES_256_GCM_SHA384","TLS_ECDHE_ECDSA_WITH_AES_256_CCM","TLS_DHE_RSA_WITH_AES_256_CCM","TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384","TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384","TLS_DHE_RSA_WITH_AES_256_CBC_SHA256","TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA","TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA","TLS_DHE_RSA_WITH_AES_256_CBC_SHA","TLS_ECDHE_ECDSA_WITH_AES_256_CCM_8","TLS_DHE_RSA_WITH_AES_256_CCM_8","TLS_ECDHE_ECDSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_ECDHE_RSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_DHE_RSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_ECDHE_ECDSA_WITH_CAMELLIA_256_CBC_SHA384","TLS_ECDHE_RSA_WITH_CAMELLIA_256_CBC_SHA384","TLS_DHE_RSA_WITH_CAMELLIA_256_CBC_SHA256","TLS_DHE_RSA_WITH_CAMELLIA_256_CBC_SHA","TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256","TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256","TLS_DHE_RSA_WITH_AES_128_GCM_SHA256","TLS_ECDHE_ECDSA_WITH_AES_128_CCM","TLS_DHE_RSA_WITH_AES_128_CCM","TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256","TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256","TLS_DHE_RSA_WITH_AES_128_CBC_SHA256","TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA","TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA","TLS_DHE_RSA_WITH_AES_128_CBC_SHA","TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8","TLS_DHE_RSA_WITH_AES_128_CCM_8","TLS_ECDHE_ECDSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_ECDHE_RSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_DHE_RSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_ECDHE_ECDSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_ECDHE_RSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_DHE_RSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_DHE_RSA_WITH_CAMELLIA_128_CBC_SHA","TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA","TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA","TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA","TLS_DHE_PSK_WITH_AES_256_GCM_SHA384","TLS_DHE_PSK_WITH_AES_256_CCM","TLS_ECDHE_PSK_WITH_AES_256_CBC_SHA384","TLS_DHE_PSK_WITH_AES_256_CBC_SHA384","TLS_ECDHE_PSK_WITH_AES_256_CBC_SHA","TLS_DHE_PSK_WITH_AES_256_CBC_SHA","TLS_DHE_PSK_WITH_CAMELLIA_256_GCM_SHA384","TLS_ECDHE_PSK_WITH_CAMELLIA_256_CBC_SHA384","TLS_DHE_PSK_WITH_CAMELLIA_256_CBC_SHA384","TLS_PSK_DHE_WITH_AES_256_CCM_8","TLS_DHE_PSK_WITH_AES_128_GCM_SHA256","TLS_DHE_PSK_WITH_AES_128_CCM","TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA256","TLS_DHE_PSK_WITH_AES_128_CBC_SHA256","TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA","TLS_DHE_PSK_WITH_AES_128_CBC_SHA","TLS_DHE_PSK_WITH_CAMELLIA_128_GCM_SHA256","TLS_DHE_PSK_WITH_CAMELLIA_128_CBC_SHA256","TLS_ECDHE_PSK_WITH_CAMELLIA_128_CBC_SHA256","TLS_PSK_DHE_WITH_AES_128_CCM_8","TLS_ECDHE_PSK_WITH_3DES_EDE_CBC_SHA","TLS_DHE_PSK_WITH_3DES_EDE_CBC_SHA","TLS_RSA_WITH_AES_256_GCM_SHA384","TLS_RSA_WITH_AES_256_CCM","TLS_RSA_WITH_AES_256_CBC_SHA256","TLS_RSA_WITH_AES_256_CBC_SHA","TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384","TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384","TLS_ECDH_RSA_WITH_AES_256_CBC_SHA","TLS_ECDH_ECDSA_WITH_AES_256_GCM_SHA384","TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384","TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA","TLS_RSA_WITH_AES_256_CCM_8","TLS_RSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_RSA_WITH_CAMELLIA_256_CBC_SHA256","TLS_RSA_WITH_CAMELLIA_256_CBC_SHA","TLS_ECDH_RSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_ECDH_RSA_WITH_CAMELLIA_256_CBC_SHA384","TLS_ECDH_ECDSA_WITH_CAMELLIA_256_GCM_SHA384","TLS_ECDH_ECDSA_WITH_CAMELLIA_256_CBC_SHA384","TLS_RSA_WITH_AES_128_GCM_SHA256","TLS_RSA_WITH_AES_128_CCM","TLS_RSA_WITH_AES_128_CBC_SHA256","TLS_RSA_WITH_AES_128_CBC_SHA","TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256","TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256","TLS_ECDH_RSA_WITH_AES_128_CBC_SHA","TLS_ECDH_ECDSA_WITH_AES_128_GCM_SHA256","TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA256","TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA","TLS_RSA_WITH_AES_128_CCM_8","TLS_RSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_RSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_RSA_WITH_CAMELLIA_128_CBC_SHA","TLS_ECDH_RSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_ECDH_RSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_ECDH_ECDSA_WITH_CAMELLIA_128_GCM_SHA256","TLS_ECDH_ECDSA_WITH_CAMELLIA_128_CBC_SHA256","TLS_RSA_WITH_3DES_EDE_CBC_SHA","TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA","TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA","TLS_RSA_PSK_WITH_AES_256_GCM_SHA384","TLS_RSA_PSK_WITH_AES_256_CBC_SHA384","TLS_RSA_PSK_WITH_AES_256_CBC_SHA","TLS_RSA_PSK_WITH_CAMELLIA_256_GCM_SHA384","TLS_RSA_PSK_WITH_CAMELLIA_256_CBC_SHA384","TLS_RSA_PSK_WITH_AES_128_GCM_SHA256","TLS_RSA_PSK_WITH_AES_128_CBC_SHA256","TLS_RSA_PSK_WITH_AES_128_CBC_SHA","TLS_RSA_PSK_WITH_CAMELLIA_128_GCM_SHA256","TLS_RSA_PSK_WITH_CAMELLIA_128_CBC_SHA256","TLS_RSA_PSK_WITH_3DES_EDE_CBC_SHA","TLS_PSK_WITH_AES_256_GCM_SHA384","TLS_PSK_WITH_AES_256_CCM","TLS_PSK_WITH_AES_256_CBC_SHA384","TLS_PSK_WITH_AES_256_CBC_SHA","TLS_PSK_WITH_CAMELLIA_256_GCM_SHA384","TLS_PSK_WITH_CAMELLIA_256_CBC_SHA384","TLS_PSK_WITH_AES_256_CCM_8","TLS_PSK_WITH_AES_128_GCM_SHA256","TLS_PSK_WITH_AES_128_CCM","TLS_PSK_WITH_AES_128_CBC_SHA256","TLS_PSK_WITH_AES_128_CBC_SHA","TLS_PSK_WITH_CAMELLIA_128_GCM_SHA256","TLS_PSK_WITH_CAMELLIA_128_CBC_SHA256","TLS_PSK_WITH_AES_128_CCM_8","TLS_PSK_WITH_3DES_EDE_CBC_SHA","TLS_EMPTY_RENEGOTIATION_INFO_SCSV"]
+  2017 - Evandro Copercini - Apache 2.0 License.
+*/
 
-#define STRAVA_API_URL "www.strava.com"
-#define AUTH_HEADER "Authorization", "Bearer " + String(authToken)
-#define ACCEPT_HEADER "Accept", "*/*"
-#define ACCEPT_ENCODING_HEADER "Accept-Encoding", "gzip, deflate"
-#define CONNECTION_HEADER "Connection", "keep-alive"
-#define HOST_HEADER "Host", "www.strava.com"
-#define USER_AGENT_HEADER "User-Agent", "ESP32StravaTracker/0.0.1"
-#define MULTIPART_BOUNDARY "fad39427494e74d088217338ea717252"
-#define MULTIPART_CONTENT_TYPE_HEADER "Content-Type", "multipart/form-data; boundary=" + String(MULTIPART_BOUNDARY)
-#define MULTIPART_FORM_KEY_PREFIX "Content-Disposition: form-data; name="
+#include <WiFiClientSecure.h>
 
-String uploadActivityFormKeys[] = {
-    "name",
-    "description",
-    "trainer",
-    "commute",
-    "data_type",
-    "external_id",
-    "file",
-};
+const char* ssid     = "UPC6530397";     // your network SSID (name of wifi network)
+const char* password = "4uh2dnyjbpGh"; // your network password
 
-const char* authToken;
-char stravaCa[2048];
-HTTPClient http;
-StaticJsonBuffer<1024> jsonBuffer;
-String readBuffer;
-String uploadPayload;
-char testGpx[4096];
+const char*  server = "www.strava.com";  // Server URL
+
+// www.howsmyssl.com root certificate authority, to verify the server
+// change it to your server root CA
+// SHA1 fingerprint is broken now!
+
+const char* test_root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDxTCCAq2gAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx\n" \
+"EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT\n" \
+"EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp\n" \
+"ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5MDkwMTAwMDAwMFoXDTM3MTIzMTIz\n" \
+"NTk1OVowgYMxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH\n" \
+"EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjExMC8GA1UE\n" \
+"AxMoR28gRGFkZHkgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIw\n" \
+"DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL9xYgjx+lk09xvJGKP3gElY6SKD\n" \
+"E6bFIEMBO4Tx5oVJnyfq9oQbTqC023CYxzIBsQU+B07u9PpPL1kwIuerGVZr4oAH\n" \
+"/PMWdYA5UXvl+TW2dE6pjYIT5LY/qQOD+qK+ihVqf94Lw7YZFAXK6sOoBJQ7Rnwy\n" \
+"DfMAZiLIjWltNowRGLfTshxgtDj6AozO091GB94KPutdfMh8+7ArU6SSYmlRJQVh\n" \
+"GkSBjCypQ5Yj36w6gZoOKcUcqeldHraenjAKOc7xiID7S13MMuyFYkMlNAJWJwGR\n" \
+"tDtwKj9useiciAF9n9T521NtYJ2/LOdYq7hfRvzOxBsDPAnrSTFcaUaz4EcCAwEA\n" \
+"AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYE\n" \
+"FDqahQcQZyi27/a9BUFuIMGU2g/eMA0GCSqGSIb3DQEBCwUAA4IBAQCZ21151fmX\n" \
+"WWcDYfF+OwYxdS2hII5PZYe096acvNjpL9DbWu7PdIxztDhC2gV7+AJ1uP2lsdeu\n" \
+"9tfeE8tTEH6KRtGX+rcuKxGrkLAngPnon1rpN5+r5N9ss4UXnT3ZJE95kTXWXwTr\n" \
+"gIOrmgIttRD02JDHBHNA7XIloKmf7J6raBKZV8aPEjoJpL1E/QYVN8Gb5DKj7Tjo\n" \
+"2GTzLH4U/ALqn83/B2gX2yKQOC16jdFU8WnjXzPKej17CuPKf1855eJ1usV2GDPO\n" \
+"LPAvTK33sefOT6jEm0pUBsV/fdUID+Ic/n4XuKxe9tQWskMJDE32p2u0mYRlynqI\n" \
+"4uJEvlz36hz1\n" \
+"-----END CERTIFICATE-----\n";
+
+// You can use x.509 client certificates if you want
+//const char* test_client_key = "";   //to verify the client
+//const char* test_client_cert = "";  //to verify the client
+
+
 WiFiClientSecure client;
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\r\n", dirname);
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(100);
 
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("- failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println(" - not a directory");
-        return;
-    }
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
 
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.name(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("\tSIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // wait 1 second for re-trying
+    delay(1000);
+  }
 
-bool readFile(fs::FS &fs, const char * path){
-    // TODO make this like here https://github.com/adjavaherian/solar-server/blob/master/lib/Poster/Poster.cpp
-    Serial.printf("Reading file: %s\r\n", path);
+  Serial.print("Connected to ");
+  Serial.println(ssid);
 
-    readBuffer = "";
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return false;
-    }
+  client.setCACert(test_root_ca);
+  //client.setCertificate(test_client_key); // for client verification
+  //client.setPrivateKey(test_client_cert);	// for client verification
 
-    Serial.println("- read from file:");
-    while(file.available()){
-        String curr = file.readString();
-        Serial.println(curr);
-        readBuffer.concat(curr);
-    }
-    file.close();
-    return true;
-}
- 
-void connectToWiFi(const char* ssid,const char* password){
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	Serial.print("Connecting to ");Serial.println(ssid);
-
-	uint8_t i = 0;
-	while (WiFi.status() != WL_CONNECTED){
-		Serial.print('.');
-		delay(500);
-
-		if ((++i % 16) == 0){
-			Serial.println(F(" still trying to connect"));
-		}
-	}
-
-	Serial.print(F("Connected. My IP address is: "));
-	Serial.println(WiFi.localIP());
-}
-
-void performStravaApiRequest(const char* method,String endpoint,String payload,String headers[],int headersCount){
-    Serial.println(payload);
-	http.begin(STRAVA_API_URL + endpoint);
-    for(int i=0;i<headersCount;i+=2){
-        http.addHeader(headers[i], headers[i+1]);
-    }
-
-	int httpCode = http.sendRequest(method,uploadPayload);
-
-	if (httpCode > 0) {
-		String payload = http.getString();
-		Serial.println(httpCode);
-		Serial.println(payload);
-	}
-	else {
-		Serial.println("Error on HTTP request");
-	}
-
-	http.end();
-}
-
-void makeUploadPayload(){
-    String uploadActivityFormVals[] = {
-        "tescisko",
-        "tesciskownie aktywne",
-        "true",
-        "false",
-        "gpx",
-        "ext_id",
-        testGpx
-    };
-
-    for(int i=0;i<7;i++){
-        String key = uploadActivityFormKeys[i];
-        String val = uploadActivityFormVals[i];
-
-        
-        uploadPayload.concat("--"+String(MULTIPART_BOUNDARY));uploadPayload.concat("\r\n");
-        uploadPayload.concat(String(MULTIPART_FORM_KEY_PREFIX) + "\""+ key+"\"");if(key=="file") uploadPayload.concat("; filename=\"test.gpx\"");uploadPayload.concat("\r\n");
-        uploadPayload.concat("\r\n");
-        uploadPayload.concat(val);uploadPayload.concat("\r\n");
-    }
-    uploadPayload.concat("\r\n");
-    uploadPayload.concat("--"+String(MULTIPART_BOUNDARY)+"--");
-    uploadPayload.concat("\r\n");
-}
-
-
-void stravaUploadWifiClient(){
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 443))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+    // Make a HTTP request:
     client.println("POST /api/v3/uploads HTTP/1.1");
     client.println("Host: www.strava.com");
-    client.println("User-Agent: ESP32Tracker");
-    client.println("Accept-Encoding: gzip, deflate");
-    client.println("Accept: */*");
     client.println("Connection: keep-alive");
-    client.println("Authorization: Bearer f7a5b4dce1460124da2874dba3c75dd276d503a9");
+    client.println("Accept: */*");
+    client.println("Authorization: Bearer f7a5b4dce1460124da2874dba3c75dd276d503a9");    
     client.println("Content-Length: 3351");
     client.println("Content-Type: multipart/form-data; boundary=08978a0a5eda7bcc6bd762556a13de1d");
     client.println();
+    
     client.println("--08978a0a5eda7bcc6bd762556a13de1d");
     client.println("Content-Disposition: form-data; name=\"name\"");
     client.println();
@@ -282,88 +205,22 @@ void stravaUploadWifiClient(){
     client.println();
     client.println("--08978a0a5eda7bcc6bd762556a13de1d--");
     client.println();
-}
 
-void setup() {
-	Serial.begin(115200);
-
-	if(!SPIFFS.begin()){ 
-		Serial.println("An Error has occurred while mounting SPIFFS");  
-	}
-    
-    if(!readFile(SPIFFS, "/strava_ca.pem")){
-        Serial.println("Could not load strava certificate file");
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
     }
-    readBuffer.toCharArray(stravaCa,readBuffer.length());
-
-        
-    if(!readFile(SPIFFS, "/config.json")){
-        Serial.println("Could not load config file");
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
     }
-    JsonObject& config = jsonBuffer.parseObject(readBuffer);
-    authToken = config["auth_token"];
-    Serial.print("Auth token is: ");Serial.println(authToken);
 
-
-    // if(!readFile(SPIFFS, "/test.gpx")){
-    //     Serial.println("Could not test gpx file");
-    // }
-    // readBuffer.toCharArray(testGpx,readBuffer.length());
-
-	connectToWiFi(config["ssid"],config["password"]);
-
-    // makeUploadPayload();
-    // Serial.println(uploadPayload);
-
-    // String getAthleteInfoHeaders[2] = {
-    //     AUTH_HEADER
-    // };
-    // String uploadActivityHeaders[16] = {
-    //     ACCEPT_HEADER,
-    //     ACCEPT_ENCODING_HEADER,
-    //     AUTH_HEADER,
-    //     CONNECTION_HEADER,
-    //     MULTIPART_CONTENT_TYPE_HEADER,
-    //     HOST_HEADER,
-    //     USER_AGENT_HEADER
-    // };
-
-
-	// performStravaApiRequest(
-    //     "POST",
-    //     "/uploads",
-    //     uploadPayload,
-    //     uploadActivityHeaders,
-    //     7
-    // );
-
-    client.setCACert(stravaCa);
-
-    if (!client.connect(STRAVA_API_URL, 443)){
-        Serial.println("Connection failed!");
-    } else {
-        Serial.println("Connected to server!");
-	    Serial.println("Performing strava request");
-        stravaUploadWifiClient();
-        
-        while (!client.available()){
-            delay(50); //
-            Serial.print(".");
-        }  
-        /* if data is available then receive and print to Terminal */
-        while (client.available()) {
-            char c = client.read();
-            Serial.write(c);
-        }
-
-        /* if the server disconnected, stop the client */
-        if (!client.connected()) {
-            Serial.println();
-            Serial.println("Server disconnected");
-            client.stop();
-        }
-    }
+    client.stop();
+  }
 }
 
 void loop() {
+  // do nothing
 }
