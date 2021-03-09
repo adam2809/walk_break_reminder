3e4a06ba5b78f74152e186c861c4d241834a5222
Adam Kulesza <adam.kule@gmail.com>
2021-03-09 18:52:16 +0100

    Moves loadConfig and parseJson to correct files

--- src/file_utils.hpp
+++ src/file_utils.hpp
@@ -1,8 +1,9 @@
+#include "json_utils.hpp"
 #include <SPIFFS.h>
 
 bool readFile(fs::FS &fs, const char * path,String& readBuffer);
 void writeFile(fs::FS &fs, const char * path, const char * message);
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
-
+JsonObject& loadConfig(DynamicJsonBuffer& jsonBuffer);
 
 
