edbec73dd86a743558f37dd9a97984b273a809bb
Adam Kulesza <adam.kule@gmail.com>
2021-03-16 19:59:48 +0100

    Adds wifi scanning functionality

--- src/networking.cpp
+++ src/networking.cpp
@@ -18,6 +18,51 @@ void connectToWiFi(const char* ssid,const char* password){
 	Serial.print(F("Connected. My IP address is: "));Serial.println(WiFi.localIP());
 }
 
+void printWifiNetworkStats(int index){
+	Serial.print(index + 1);
+	Serial.print(": ");
+	Serial.print(WiFi.SSID(index));
+	Serial.print(" (");
+	Serial.print(WiFi.RSSI(index));
+	Serial.print(")");
+	Serial.println((WiFi.encryptionType(index) == WIFI_AUTH_OPEN)?" ":"*");
+}
+
+
+int indexOfSsidInWifiArr(JsonArray& arr,String ssid){
+	for (int i = 0; i < arr.size(); i++){
+		String currSsid = arr.get<JsonObject&>(i)["ssid"];
+		if(arr.get<JsonObject&>(i)["ssid"].as<String>() == ssid){
+			return i;
+		}
+	}
+	return -1;
+}
+
+/*
+Scans for networks and returns index of found wifi network or -1 in none found
+*/
+int scanForSavedWifiNetworks(JsonArray& savedWiFiNetworks){
+    Serial.println("Starting to scan for networks");
+    int n = WiFi.scanNetworks();
+
+    if (n == 0) {
+        Serial.println("No networks found");
+		return -1;
+    }
+
+	Serial.print(n);
+	Serial.println(" networks found:");
+	for (int i = 0; i < n; ++i) {
+		printWifiNetworkStats(i);
+		String currSsid = WiFi.SSID(i);
+		if(indexOfSsidInWifiArr(savedWiFiNetworks,currSsid) != -1){
+			return i;
+		}
+	}
+	return -1;
+}
+
 String performSupportServerRequest(HTTPClient& http,const char* method,String endpoint,String requestParams,String headers[],int headersCount,String payload){
     Serial.println(payload);
 	http.begin(SUPPORT_SERVER_URL + endpoint);
--- src/networking.hpp
+++ src/networking.hpp
@@ -1,7 +1,9 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
 
 #define SUPPORT_SERVER_URL "http://192.168.0.87:8000"
 
 void connectToWiFi(const char* ssid,const char* password);
 String performSupportServerRequest(HTTPClient& http,const char* method,String endpoint,String requestParams,String headers[],int headersCount,String payload);
+int scanForSavedWifiNetworks(JsonArray& savedWiFiNetworks);
